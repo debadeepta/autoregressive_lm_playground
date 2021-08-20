ed7f2d666649af9be2d9de6e06e9b188708de1be
Debadeepta Dey <dedey@microsoft.com>
2021-08-20 00:20:00 +0000

    VW example creating nominally working.

--- create_vw_lm_dataset.py
+++ create_vw_lm_dataset.py
@@ -1,17 +1,64 @@
 import math
 import argparse
-from typing import Tuple
+from typing import Tuple, Dict, List
+from collections import defaultdict
+from tqdm import tqdm
+import os
 
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.utils.data import dataset
-#from torchtext.datasets import WikiText2
+from torchtext.datasets import WikiText2
 from torchtext.datasets import WikiText103
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 
+
+def create_vw_examples(raw_text_iter: dataset.IterableDataset, 
+                        context_length:int, 
+                        stoe:Dict[str, List[float]], 
+                        stoi:Dict[str, int],
+                        vw_file_name: str):
+    # extract character sequences of context length and the next target character
+    char_seqs = []
+    char_targets = []
+    for item in raw_text_iter:
+        chars = list(item.rstrip('\n'))
+        if len(chars) < context_length + 1:
+            continue
+        else:
+            end = len(chars) - context_length
+            for s in range(end):
+                seq = chars[s:s+context_length]
+                target = chars[s+context_length]
+                char_seqs.append(seq)
+                char_targets.append(target)
+
+    print(f'created {len(char_seqs)} examples')
+
+    # featurize each example
+    with open(vw_file_name, 'wt') as f:
+        for char_seq, char_target in tqdm(zip(char_seqs, char_targets)):
+            target_idx = stoi[char_target]
+            if target_idx == -1:
+                continue
+            feature_string = ''
+            feature_list = []
+            for char in char_seq:
+                embed = stoe[char]
+                if embed == 'missing':
+                    continue
+                feature_list.extend(embed)
+            for idx, feat in enumerate(feature_list):
+                feature_string += str(idx) + ':' + feat + ' '
+            example_string = str(target_idx) + ' ' + '|'  + ' '  + feature_string + '\n'
+            f.write(example_string)
+        
+
+
+    
 
 
 
@@ -26,17 +73,32 @@ def main():
                         help='number of last character embeddings to use as features')
     args, extra_args = parser.parse_known_args()
 
+    os.makedirs(args.output_dir, exist_ok=True)
+
     # load the embeddings
+    stoi = defaultdict(lambda: -1)
+    itos = defaultdict(lambda: 'missing')
+    stoe = defaultdict(lambda: 'missing')
+    itoe = defaultdict(lambda: 'missing')
     with open(args.vocab_embeddings_file, 'r') as f:
         embed_lines = f.readlines()
+        for line in embed_lines:
+            temp = eval(line)
+            idx = temp['idx']
+            token = temp['token']
+            embed = temp['embed']
+            stoi[token] = idx
+            itos[idx] = token
+            stoe[token] = embed
+            itoe[idx] = embed
+
+
+    # create vw examples
+    train_iter, val_iter, test_iter = WikiText2()
+    vw_file_name = os.path.join(args.output_dir, 'wikitext2_train_vw.txt')
+    create_vw_examples(train_iter, args.context_length, stoe, stoi, vw_file_name)    
+
     
-    
-
-
-
-    # load the WikiText103 dataset
-    train_iter = WikiText103(split='train')    
-
 
 if __name__ == '__main__':
     main()
